ef0263efea6dd17a3378c35b8622efc576a4585b
theprotos <vlcz@seznam.cz>
2020-05-05 20:26:16 +0200

    linux: updated build and bootstrap

--- linux/build.ps1
+++ linux/build.ps1
@@ -1,5 +1,35 @@
-function Vagrant-Package
-{
+new-module -name Build-LinuxImage -scriptblock {
+
+##Requires -RunAsAdministrator
+
+<#
+        .\build.ps1 | iex; show-help
+        .\build.ps1 | iex; build-image -packerConfig .\template\win-2019-1909-dc.json
+     #>
+#>
+
+function Show-Help {
+    <#
+    .SYNOPSIS
+        Helper.
+    #>
+    Write-Host "
+        TOPIC
+            Dynamic Module Build-LinuxImage.
+
+        SHORT DESCRIPTION
+            Build vagrant image for Linux OS
+
+        USAGE DESCRIPTION
+            build.ps1 [-imageVersion <version>] [-imageName <linux-generic>] [-imageDescription <image description>]
+
+        USAGE EXAMPLES:
+            .\build.ps1 | Build-Image -imageVersion 1.2.3.4 -imageName linux-generic -imageDescription 'basic linux image'
+
+        "
+}
+
+function Vagrant-Package {
     $version = Get-Date -Format "yyyy.MMdd.HHmm"
     $metadata = "{
       ""name"": ""linux-generic"",
@@ -12,51 +42,57 @@ function Vagrant-Package
           }]
       }]
     }"
-    try
-    {
+    try {
         $metadata | Out-File metadata.json -Encoding ASCII
         Write-Host "$( Get-Date -Format 'yyyy-MM-dd HH:mm' ) ========[ Vagrant: Create box ]========"
         vagrant package
     }
-    catch
-    {
+    catch {
         Vagrant-Cleanup
         Write-Error "$( $_.exception.message )"
         throw $_.exception
     }
 }
 
-function Vagrant-Cleanup
-{
+function Vagrant-Cleanup {
     vagrant destroy -f
     Write-Host "$( Get-Date -Format 'yyyy-MM-dd HH:mm' ) ========[ Vagrant: Clean up ]========"
     Remove-Item -force -recurse -ErrorAction SilentlyContinue metadata.json
     Remove-Item -force -recurse -ErrorAction SilentlyContinue package.box
     vagrant box prune
+    cd ..
 }
 
 # Init
-Write-Host "$( Get-Date -Format 'yyyy-MM-dd HH:mm' ) ========[ Vagrant: STARTED ]========"
-cd template
-try
-{
+function Build-Image {
+
+    Param(
+        $imageVersion = (Get-Date -Format "yyyy.MMdd.HHmm" -ErrorAction SilentlyContinue),
+        $imageName = "Linux generic",
+        $imageDescription = "no description"
+    )
+
+    Write-Host "$( Get-Date -Format 'yyyy-MM-dd HH:mm' ) ========[ Vagrant: STARTED ]========"
+    cd template
+    try {
+        Vagrant-Cleanup
+        vagrant up
+        Vagrant-Package
+    }
+    catch {
+        Vagrant-Cleanup
+        Write-Error "$( $_.exception.message )"
+        throw $_.exception
+    }
+
+    Write-Host "$( Get-Date -Format 'yyyy-MM-dd HH:mm' ) ========[ Vagrant: Add box ]========"
+    vagrant box add metadata.json
+
+    Write-Host "$( Get-Date -Format 'yyyy-MM-dd HH:mm' ) ========[ Vagrant: List of boxes ]========"
+    vagrant box list
+
     Vagrant-Cleanup
-    vagrant up
-    Vagrant-Package
-}
-catch
-{
-    Vagrant-Cleanup
-    Write-Error "$( $_.exception.message )"
-    throw $_.exception
-}
 
-Write-Host "$( Get-Date -Format 'yyyy-MM-dd HH:mm' ) ========[ Vagrant: Add box ]========"
-vagrant box add metadata.json
-
-Write-Host "$( Get-Date -Format 'yyyy-MM-dd HH:mm' ) ========[ Vagrant: List of boxes ]========"
-vagrant box list
-
-Vagrant-Cleanup
-cd ..
-
+    Export-ModuleMember -function 'Build-Image' #-alias 'build-image'
+    Export-ModuleMember -function 'Show-Help' #-alias 'get-help'
+}}
--- linux/template/bootstrap-template.sh
+++ linux/template/bootstrap-template.sh
@@ -5,14 +5,14 @@
 #tflint="https://github.com/terraform-linters/tflint/releases/latest/download/tflint_linux_amd64.zip"
 tfdocs="https://github.com/segmentio/terraform-docs/releases/latest/download/terraform-docs-v0.6.0-linux-amd64"
 awsvault="https://github.com/99designs/aws-vault/releases/latest/download/aws-vault-linux-amd64"
-direnv="https://github.com/direnv/direnv/releases/latest/download/direnv.linux-amd64"
+#direnv="https://github.com/direnv/direnv/releases/latest/download/direnv.linux-amd64"
 #vault="https://releases.hashicorp.com/vault/1.1.5/vault_1.1.5_linux_amd64.zip"
 #helm="https://get.helm.sh/helm-v3.0.0-linux-amd64.tar.gz"
 #minikube="https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64"
 #####################################################
 
-printf "    ========[ YUM: Install common packages ]========"
-curl -sL https://raw.githubusercontent.com/theprotos/cookbooks-generic/development/scripts/linux.sh | sudo bash -s apply linux-vagrant.json,linux-packages.json,linux-docker.json development
+printf "$(date +%T) ========[ YUM: Install common packages ]========"
+curl -sL https://raw.githubusercontent.com/theprotos/cookbooks-generic/development/scripts/linux.sh | sudo bash -s apply linux-vagrant.json,linux-k8s.json development
 
 pip3 install -q pre-commit
 
@@ -25,7 +25,6 @@ function bin_install {
 
 bin_install $tfdocs terraform-docs
 bin_install $awsvault aws-vault
-bin_install $direnv direnv
 
 function bin_install_zip {
     printf "    ========[ Install $1 ]========"
@@ -38,6 +37,6 @@ function bin_install_zip {
 }
 
 
-curl -fLo /usr/bin/yadm https://github.com/TheLocehiliosan/yadm/raw/master/yadm && chmod a+x /usr/bin/yadm
+#curl -fLo /usr/bin/yadm https://github.com/TheLocehiliosan/yadm/raw/master/yadm && chmod a+x /usr/bin/yadm
 
 exit 0
